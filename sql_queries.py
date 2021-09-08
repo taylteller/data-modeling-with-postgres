f6d9ee6e963479f2f5ead246b654ca0481ec839a
Taylor Najjar <16545112+taylteller@users.noreply.github.com>
2021-09-08 15:21:44 -0400

    table creation

--- sql_queries.py
+++ sql_queries.py
@@ -1,26 +1,67 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id serial PRIMARY KEY, 
+    start_time timestamp, 
+    user_id integer, 
+    level varchar, 
+    song_id varchar, 
+    artist_id varchar, 
+    session_id integer, 
+    location varchar, 
+    user_agent varchar
+);
 """)
 
 user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users (
+    user_id integer PRIMARY KEY, 
+    first_name varchar, 
+    last_name varchar, 
+    gender varchar, 
+    level varchar
+);
 """)
 
 song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs (
+    song_id varchar PRIMARY KEY, 
+    title varchar, 
+    artist_id varchar, 
+    year integer, 
+    duration decimal
+);
 """)
 
 artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id varchar PRIMARY KEY, 
+    name varchar, 
+    location varchar, 
+    latitude decimal, 
+    longitude decimal
+);
 """)
 
 time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time (
+    start_time timestamp PRIMARY KEY, 
+    hour integer, 
+    day integer, 
+    week integer, 
+    month integer, 
+    year integer, 
+    weekday integer
+);
 """)
 
 # INSERT RECORDS
